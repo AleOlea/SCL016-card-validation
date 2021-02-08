bc2522f8c2b9204bb2cfbc4d64dd5d59e5615ec5
AleOlea <espiritusdelfuego@gmail.com>
2021-02-08 08:56:18 -0300

    double number working

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = iniciar;
+
+function iniciar(){
+   let btnAgregar = document.getElementById("btnAgregar");
+   btnAgregar.addEventListener("click", clickBtnAgregar)
+   mostrarNotas();
+}
+
+function clickBtnAgregar() {
+    let txtNota = document.getElementById("txtNota");
+
+    let notas = [];
+    notas.push(textNota.Value);
+    localStorage.notas = JSON.stringify(notas);
+    mostrarNotas();
+}
+
+function mostrarNotas() {
+    let divNotas = document.getElementById("letNotas");
+    let notas = JSON.parse(localStorage.notas);
+    for(var nota of notas)
+    letNotas.innerHTML = localStorage.nota;
+
+
+}
--- src/index.js
+++ src/index.js
@@ -10,102 +10,47 @@ para actualizar resultado en pantalla (UI).*/
 //load page and execute function
 window.onload = iniciar;
 
-
 function iniciar() {
 let btnValidar = document.getElementById("btnValidar");
-btnValidar.addEventListener("click", clickbtnValidar);/*"clickbtnValidar" function, created line 21*/
+btnValidar.addEventListener("click", clickbtnValidar);/*"clickbtnValidar" function created line 18*/
 };
 
 function clickbtnValidar(event) {
     event.preventDefault();
     let creditCard = document.getElementById("creditCard").value;
+
+  
+
+
     console.log(creditCard);
     /*alert("validando card nro:" + creditCard)*/
 
-    
- 
+//To see the number on screen//
+    let detalle = document.getElementById("detalle");
+    detalle.innerHTML = creditCard;
+
+
     let digits = creditCard.split("");
     if (digits.length % 2 === 0) {
         digits = digits.map((digit, idx) => idx % 2 === 0 ? digit * 2 : digit);
       } else {
         digits = digits.map((digit, idx) => idx % 2 === 1 ? digit * 2 : digit);
       }
-      
-    console.log(digits)
-    
-   
+      console.log(digits)  
 }
 
 
-/*
-for(let i = 0; i < creditCard.length; i++){
+
+
+
+
+
+//testing for masking//
+/*for(let i = 0; i < creditCard.length; i++){
     let card = creditCard[i];
     /*console.log(i)*/
 
 
-
-
-
-
-
-
-
-
-
-
-    
-    
-    // function to display card number on screen, it is not escential
-    /*function showCard() {
-        let numbers = document.getElementById("numbers");
-        let html = " ";
-        for (let creditCard of digits) {
-            html += creditCard;
-            
-        }
-        showCard.innerHtml = html;
-        
-    }*/
-    
-
-
-        
-   
-
-
-    
-
-//Codigo que me dio domi para que  no se fuera función, la palabra "event" y la linea event.preventDefault()"
-/*function clickbtnValidar(event) {
-    event.preventDefault();" */
-
-
-/* OBJECTIVE:
-HOW I GET THE INPUT NUMBER.
-HOW DO I WORK WITH THAT NUMBER TO TRANSFORM IN STRING, SPLIT, INVERSE?.
-
-
-/*PSEUDO CODIGO
-   //BOILER PLATE//
-    const validator = {
- //BOILER PLATE// 
-
-    isValid(ccValue) {
-
-    let digits = ccValue.split("").
-
-        const ccSplit= function (ccValue) {
-        ccValue.toString()
-        ccSplit()
-        inverse()
-        map() with luhn function
-    }
-
-      };
-      
-      export default validator;
-      */
-
-
+      /*export default validator;*/
 
 

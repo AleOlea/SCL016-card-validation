26cf0e08484045fc60bc3d94bd9d3fe6b4d62239
AleOlea <espiritusdelfuego@gmail.com>
2021-02-10 13:31:22 -0300

    working maskify

--- src/index.js
+++ src/index.js
@@ -3,30 +3,31 @@ import validator from './validator.js';
 let btnValidar = document.getElementById("btnValidar");
 btnValidar.addEventListener("click", clickbtnValidar);
 
-function clickbtnValidar(event){
+function clickbtnValidar(event) {
   event.preventDefault();
   let creditCard = document.getElementById("creditCard").value;
+  
   console.log(creditCard);
 
+  document.getElementById("detalle").innerText = validator.isValid(creditCard);
 
-  document.getElementById("detalle").innerText =  validator.isValid(creditCard);
 };
- 
-  
 
 
 
 
 
 
-  
-
-
- 
 
 
 
- 
+
+
+
+
+
+
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -2,14 +2,15 @@ const validator = {
 
   isValid: function (creditCard) {
 
-    let digits = creditCard.split("")     //eliminé reverse() pues daba error en tarjetas orobadas que eran validas//
+    let digits = creditCard.split("")    //eliminé reverse() pues daba error en tarjetas validas//
     let sum = 0;
 
     for (let i = 0; i < creditCard.length; i++) {
       digits[i] = parseInt(digits[i]);
       console.log(digits[i])
 
-      // NO ESTA FUNCIONANDO
+//PREGUNTA 1 NO ESTA FUNCIONANDO//
+
       if (creditCard.value < 10) {
         alert("Please, enter a Credit Card Number");
       }
@@ -20,34 +21,39 @@ const validator = {
           digits[i] -= 9
         }
       }
-
       sum += digits[i];
       console.log(digits[i]);
     }
+
     console.log(sum);
     if (sum % 10 === 0) {
       console.log("Valid card")
       detalle.style.color = "#00ff00";
-      return "Valid Card" 
-     }
+      return "Valid Card"
+    }
+
     else {
       console.log("Not Valid")
       detalle.style.color = "red";
       return " Not Valid";
-       }
-  
-      }
     }
+  },
+//PREGUNTA 2 NO FUNCIONA//
+
+  maskify(creditCard) {
+    let digitsArray = creditCard;
+    for (let i = 0; i < creditCard.length; i++) {
+      digitsArray[i] = "#";
+    }
+    console.log(digitsArray);
+  }
+};
+
 export default validator;
 
 
 
-//MASKIFY
-/*
-let digitsToHide= creditCard.value;
-let lastDigit = digitsToHide.slice(-4);
-let mask_symbol = "#";
-let masked_str = mask_symbol.repeat() + lastDigit;*/
+
 
 
 

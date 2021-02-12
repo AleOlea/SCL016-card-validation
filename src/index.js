fb27971f43f3d90b6ce7e134664c831d3b464d76
AleOlea <espiritusdelfuego@gmail.com>
2021-02-11 21:14:55 -0300

    Working Masking

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
 import validator from './validator.js';
 
+//Getting the element that listen to the event to triger the action (funtion)
 let btnValidar = document.getElementById("btnValidar");
 btnValidar.addEventListener("click", clickbtnValidar);
 
+//Defining the function that gets the user input value.
 function clickbtnValidar(event) {
   event.preventDefault();
   let creditCardNumber = document.getElementById("creditCard").value;
 
   console.log(creditCardNumber);
+  //variable created to deliver colors red and green for the results.
   let validResult = validator.isValid(creditCardNumber);
-
   document.getElementById("detalle").innerText = validResult;
-
   if (validResult === "Valid Card") {
     detalle.style.color = "#00ff00";
   } else {
     detalle.style.color = "red";
   }
+  //To ask the user for an appropiated credit card number//
 
-  if (creditCardNumber.length < 13) {
+  /*if (creditCardNumber.length < 13) {
     alert("Please enter a card number");
-  }
+  }*/
 
-  //refresh the box
+  //To refresh the box "Credit Card" using "Start Again".
+
+  let refresh = document.getElementById("refresh");
+
+  refresh.addEventListener("click", function () {
+  document.getElementById("detalle").innerText = "Result";
+  detalle.style.color = "black";
+
+  document.getElementById("creditCard").value = "";//to have an empty input field
+  })
+
+  //To maskify the credit card number input on the box "Credit Card"
 
 
-
-    let refresh = document.getElementById("refresh");
-    refresh.addEventListener("click", function(){
-    document.getElementById("detalle").innerText= "Result";
-    detalle.style.color = "black";
-    document.getElementById("creditCard").value = "";//empty input field
-    })
-  
-    let maskD = validator.maskify(creditCardNumber);
-    document.getElementById("creditCard").value= maskD;
-    console.log(maskD)
+  let maskD = validator.maskify(creditCardNumber);
+  document.getElementById("creditCard").value = maskD;
+  console.log(maskD)
 
 };
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -29,7 +29,7 @@ const validator = {
 
     else {
       console.log("FALSE")
-      return " Not Valid";
+      return "Not Valid";
     }
   },
 
@@ -41,63 +41,29 @@ const validator = {
     console.log(maskDigits)
     return maskDigits.join("");
   }
+
+  
  
 };
 
 export default validator;
 
 
+//Código de cami
 
-
-
-
-//FUNCION ORIGINAL//
-/*const validator = {
-  isValid: function (creditCard) {
-
-    let digits = creditCard.split("");    //eliminé reverse() pues daba error en tarjetas validas//
-    let sum = 0;
-
-    for (let i = 0; i < creditCard.length; i++) {
-      digits[i] = parseInt(digits[i]);
-      console.log(digits[i])
-
-    if (i % 2 === 0) {
-        digits[i] *= 2
-
-    if (digits[i] > 9) {
-          digits[i] -= 9
-          //testing adding digits
-          /*let newDigit = digit.split("").map(Number);
-          digit[i] = newDigit[0] + newDigit[1]
-          console.log(newDigit)
-        }
-      }
-
-      sum += digits[i];
-      console.log(digits[i]);
-      }
-
-    console.log(sum);
-    if (sum % 10 === 0) {
-      console.log("TRUE")
-      detalle.style.color = "#00ff00";
-      return "Valid Card"
-    }
-
-    else {
-      console.log("FALSE")
-      detalle.style.color = "red";
-      return " Not Valid";
-    }
-
-};
-
-export default validator;*/
-
-
-
-
+/*
+maskify(creditCard) {
+  let maskD = "";
+  let maskDigits = creditCard.split("");
+  for (let i = 0; i < maskDigits.length -4; i++) {
+    if(i< maskDigits.length -4){
+      maskDigits[i]= "#";
+}
+maskD = maskD + maskD [i]; 
+}
+return maskD;
+ }
+*/
 
 
 
